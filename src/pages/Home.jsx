9015d48218f0806870fa1b19f9297dbcaa5d7582
Rahil Ahmed <rahilahmed1720@gmail.com>
2025-01-26 08:30:30 +0530

    fix home responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,16 +11,16 @@ import Atom from "../components/core/Homepage/Atom";
 const Home = () => {
   return (
     <div>
-      <div className="relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between gap-8">
+      <div className="relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between md:gap-8">
         {/* Section 1 */}
-        <div className="flex justify-between items-center my-16">
-          <div className="w-[50%]">
-            <div className="text-4xl text-center font-semibold mb-7">
+        <div className="flex flex-col md:flex-row justify-between items-center my-16">
+          <div className="w-[100%] md:w-[50%]">
+            <div className="text-3xl sm:text-4xl text-center font-semibold mb-7">
               Master Skills,
-              <HighlightText text={"One Step at a Time"} />
+              <HighlightText text={" One Step at a Time"} />
             </div>
 
-            <div className="text-lg font-bold text-center text-richblack-300">
+            <div className="text-md sm:text-lg font-bold text-center text-richblack-300">
               Your ultimate destination for top-notch, curated courses to
               elevate your skills and career. Learn at your own pace, track your
               progress, and earn rewards as you upskill. With SkillStep, every
@@ -38,31 +38,35 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="w-[50%]">
-            <img src={Home1} alt="Home1" />
+          <div className="w-[100%] md:w-[50%] flex justify-center items-center mt-16 md:mt-0">
+            <img src={Home1} width={"80%"} alt="success" />
           </div>
         </div>
 
         {/* Section 2 */}
-        <div className="flex flex-col-reverse sm:flex-row justify-center items-center mt-10 gap-10 sm:gap-20 mb-10">
+        <div className="flex flex-col-reverse md:flex-row justify-center items-center mt-10 md:mt-0 gap-10 sm:gap-20 mb-10">
           {/* left part */}
-          <div className="w-1/2 sm:w-[38%]">
-            <img src={Home2} height={300} alt="" />
+          <div className="w-[100%] md:w-[50%] flex justify-center items-center">
+            <img
+              src={Home2}
+              alt="progress_track"
+              className="w-[60%] md:w-[90%]"
+            />
           </div>
 
           {/* left part */}
-          <div className="w-full sm:w-1/2 flex flex-col gap-6">
-            <div className="text-4xl font-semibold mt-10">
+          <div className="w-[100%] md:w-[50%] flex flex-col gap-6">
+            <div className="text-3xl sm:text-4xl font-semibold mt-10">
               Structured
               <HighlightText text={" Learning "} />
               Path
             </div>
 
-            <div className="w-[90%] text-xl font-bold text-blue-50s">
+            <div className="w-[90%] text-lg md:text-xl font-bold text-blue-50s">
               Stay Consistent and Master New Skills Effectively
             </div>
 
-            <div className="w-[90%] text-lg font-bold text-richblack-300">
+            <div className="w-[90%] text-md md:text-lg font-bold text-richblack-300">
               Follow a guided, alternate-day structure to maintain consistency
               and make steady progress. Each course is designed to keep you
               engaged and motivated, without overwhelming you.
@@ -77,19 +81,19 @@ const Home = () => {
         </div>
 
         {/* Section 3 */}
-        <div className="flex flex-col-reverse sm:flex-row justify-center items-center mt-10 gap-10 sm:gap-20 mb-10">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-10 gap-10 sm:gap-20 mb-10">
           {/* left part */}
-          <div className="w-full sm:w-1/2 flex flex-col gap-6">
-            <div className="text-4xl font-semibold mt-10">
+          <div className="w-[100%] md:w-[50%] flex flex-col gap-6">
+            <div className="text-3xl sm:text-4xl font-semibold mt-10">
               Progress
               <HighlightText text={" Tracking & Certificates"} />
             </div>
 
-            <div className="w-[90%] text-xl font-bold text-blue-50s">
+            <div className="w-[90%] text-lg sm:text-xl font-bold text-blue-50s">
               Monitor Your Progress and Showcase Your Achievements
             </div>
 
-            <div className="w-[90%] text-lg font-bold text-richblack-300">
+            <div className="w-[90%] text-md sm:text-lg font-bold text-richblack-300">
               Stay on top of your learning journey with personalized dashboards
               and earn certificates upon course completion to highlight your
               accomplishments.
@@ -103,30 +107,40 @@ const Home = () => {
           </div>
 
           {/* right part */}
-          <div className="w-1/2 sm:w-[38%]">
-            <img src={Home3} height={300} alt="" loading="lazy" />
+          <div className="w-[100%] md:w-[50%] flex justify-center items-center">
+            <img
+              src={Home3}
+              alt="certificate"
+              loading="lazy"
+              className="w-[60%] md:w-[90%]"
+            />
           </div>
         </div>
 
         {/* Section 4 */}
-        <div className="flex flex-col-reverse sm:flex-row justify-center items-center gap-10 sm:gap-20">
+        <div className="flex flex-col-reverse md:flex-row justify-center items-center gap-10 md:gap-10">
           {/* left part */}
-          <div className="w-1/2 sm:w-[38%]">
-            <img src={Home4} height={300} alt="" loading="lazy" />
+          <div className="w-[100%] md:w-[50%] flex justify-center items-center">
+            <img
+              src={Home4}
+              alt="games"
+              loading="lazy"
+              className="w-[70%] md:w-[90%]"
+            />
           </div>
 
           {/* right part */}
-          <div className="w-full sm:w-1/2 flex flex-col gap-6">
-            <div className="text-4xl font-semibold mt-10">
+          <div className="w-[100%] md:w-[50%] flex flex-col gap-6">
+            <div className="text-3xl sm:text-4xl font-semibold mt-10">
               Gamification for
               <HighlightText text={" Motivation"} />
             </div>
 
-            <div className="w-[90%] text-xl font-bold text-blue-50s">
+            <div className="w-[90%] text-lg sm:text-xl font-bold text-blue-50s">
               Stay Engaged and Motivated with Fun Rewards
             </div>
 
-            <div className="w-[90%] text-lg font-bold text-richblack-300">
+            <div className="w-[90%] text-md sm:text-lg font-bold text-richblack-300">
               Earn badges, points, and climb the leaderboard as you progress
               through your learning journey. Keep up the streak and be rewarded
               for your dedication and hard work!
@@ -141,19 +155,19 @@ const Home = () => {
         </div>
 
         {/* Section 5 */}
-        <div className="flex flex-col-reverse sm:flex-row justify-center items-center mt-10 gap-10 sm:gap-20 mb-10">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-10 gap-10 sm:gap-20 mb-10">
           {/* left part */}
-          <div className="w-full sm:w-1/2 flex flex-col gap-6">
-            <div className="text-4xl font-semibold mt-10">
+          <div className="w-[100%] md:w-[50%] flex flex-col gap-6">
+            <div className="text-3xl sm:text-4xl font-semibold mt-10">
               Email
               <HighlightText text={" Notifications"} />
             </div>
 
-            <div className="w-[90%] text-xl font-bold text-blue-50s">
+            <div className="w-[90%] text-lg sm:text-xl font-bold text-blue-50s">
               Stay Informed and On Track with Timely Updates
             </div>
 
-            <div className="w-[90%] text-lg font-bold text-richblack-300">
+            <div className="w-[90%] text-md sm:text-lg font-bold text-richblack-300">
               Receive timely updates and reminders about new content and
               courses, tailored to your learning preferences. Stay informed and
               never miss an opportunity to enhance your skills.
@@ -167,8 +181,13 @@ const Home = () => {
           </div>
 
           {/* right part */}
-          <div className="w-1/2 sm:w-[38%]">
-            <img src={Home5} height={300} alt="" loading="lazy" />
+          <div className="w-[100%] md:w-[50%] flex justify-center items-center">
+            <img
+              src={Home5}
+              alt="email_notification"
+              loading="lazy"
+              className="w-[70%] md:w-[90%]"
+            />
           </div>
         </div>
 
@@ -181,16 +200,16 @@ const Home = () => {
 
           {/* left part */}
           <div className="w-full lg:w-1/2 flex flex-col gap-6 text-wrap">
-            <div className="text-4xl font-semibold mt-10">
+            <div className="text-3xl sm:text-4xl font-semibold mt-10">
               Unlock Your
               <HighlightText text={" Future"} />
             </div>
 
-            <div className="w-[90%] text-xl font-bold text-blue-50s">
+            <div className="w-[90%] text-lg sm:text-xl font-bold text-blue-50s">
               Transform Your Learning into Opportunities
             </div>
 
-            <div className="w-[90%] text-lg font-bold text-richblack-300">
+            <div className="w-[90%] text-md sm:text-lg font-bold text-richblack-300">
               At SkillStep, we equip you with the tools and resources to achieve
               your career goals through structured courses and a supportive
               community. Gain in-demand skills, access tailored content, earn
